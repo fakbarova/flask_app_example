05da74517a4403d7072608da19ecca7db1a58428
fakbarova <fakbarova462@gmail.com>
2024-11-26 12:50:46 +0200

    Create everyday_books.py

---
+++ everyday_books.py
@@ -0,0 +1,26 @@
+books = [
+{ 
+    "title": "Gone with the Wind",
+    "author": "Margaret Mitchell",
+    "year": 1936,
+    "genre": "Romance Novel"
+},
+{
+    "title": "Rich dad Poor dad",
+    "author": "Robert Kiyosaki",
+    "year": 1997,
+    "genre": "Finance"
+},
+{
+    "title": "Who Took my Money?",
+    "author": "Robert Kiyosaki",
+    "year": 2004,
+    "genre": "Finance"
+},
+{ 
+    "title": "When I return be at home",
+    "author": "Elchin Safarli ",
+    "year": 2022,
+    "genre": "Fiction"
+}
+]
